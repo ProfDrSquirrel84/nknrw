9769d5fd705fc848bf53004c6719978b35be31d9
ProfDrSquirrel84 <eichhornse@googlemail.com>
2026-09-18 11:05:43 +0200

    Update app_nrw_karte.py

--- app_nrw_karte.py
+++ app_nrw_karte.py
@@ -606,7 +606,7 @@ with col_map:
     map_output = st_folium(
         m,
         width="100%",
-        height=820,
+        height=1280,
         returned_objects=["last_active_drawing"],
     )
     st.markdown('</div>', unsafe_allow_html=True)
